79af6eca77a594a763d9af4bbc56c002c5a47133
Weiberle <maxweiberle63@gmail.com>
2023-02-11 14:36:40 +0100

    formatting

--- dictionary/uebung2.py
+++ dictionary/uebung2.py
@@ -1,25 +1,25 @@
 blitzer = [
-    {"uhrzeit": "12:43:17", "geschwindigkeit": 37, "kennzeichen": "TÜ-TL1234"},
-    {"uhrzeit": "12:44:01", "geschwindigkeit": 35, "kennzeichen": "TÜ-FL999"},
-    {"uhrzeit": "12:44:57", "geschwindigkeit": 45, "kennzeichen": "RT-ZZ321"},
-    {"uhrzeit": "12:47:13", "geschwindigkeit": 51, "kennzeichen": "K-FC48"},
-    {"uhrzeit": "12:44:57", "geschwindigkeit": 38, "kennzeichen": "RT-R777"},
-    {"uhrzeit": "12:51:17", "geschwindigkeit": 42, "kennzeichen": "TÜ-XY1"},
-    {"uhrzeit": "12:51:29", "geschwindigkeit": 40, "kennzeichen": "LEO-Z4"},
-    {"uhrzeit": "12:54:00", "geschwindigkeit": 43, "kennzeichen": "TÜ-AB656"},
+  {"uhrzeit": "12:43:17", "geschwindigkeit": 37, "kennzeichen": "TÜ-TL1234"},
+  {"uhrzeit": "12:44:01", "geschwindigkeit": 35, "kennzeichen": "TÜ-FL999"},
+  {"uhrzeit": "12:44:57", "geschwindigkeit": 45, "kennzeichen": "RT-ZZ321"},
+  {"uhrzeit": "12:47:13", "geschwindigkeit": 51, "kennzeichen": "K-FC48"},
+  {"uhrzeit": "12:44:57", "geschwindigkeit": 38, "kennzeichen": "RT-R777"},
+  {"uhrzeit": "12:51:17", "geschwindigkeit": 42, "kennzeichen": "TÜ-XY1"},
+  {"uhrzeit": "12:51:29", "geschwindigkeit": 40, "kennzeichen": "LEO-Z4"},
+  {"uhrzeit": "12:54:00", "geschwindigkeit": 43, "kennzeichen": "TÜ-AB656"},
 ]
 
 ergebnisDict = {}
 
 for blitz in blitzer:
-    i = blitz["kennzeichen"].index("-")
-    bezirk = blitz["kennzeichen"][0:i]
-    if bezirk in ergebnisDict:
-        ergebnisDict[bezirk].append(blitz["geschwindigkeit"])
-    else:
-        ergebnisDict[bezirk] = [blitz["geschwindigkeit"]]
+  i = blitz["kennzeichen"].index("-")
+  bezirk = blitz["kennzeichen"][0:i]
+  if bezirk in ergebnisDict:
+    ergebnisDict[bezirk].append(blitz["geschwindigkeit"])
+  else:
+    ergebnisDict[bezirk] = [blitz["geschwindigkeit"]]
 
 for bezirk, geschwindigkeiten in ergebnisDict.items():
-    ergebnisDict[bezirk] = sum(geschwindigkeiten) / len(geschwindigkeiten)
+  ergebnisDict[bezirk] = sum(geschwindigkeiten) / len(geschwindigkeiten)
 
 print(ergebnisDict)
--- kontrollstrukturen/uebung2.py
+++ kontrollstrukturen/uebung2.py
@@ -2,19 +2,19 @@ personen = 5
 putzliste = []
 
 for x in range(personen):
-    putzliste.append([])
+  putzliste.append([])
 
 for x in range(1, 366):
-    if x%5 == 1:
-        putzliste[0].append(x)
-    elif x%5 == 2:
-        putzliste[1].append(x)
-    elif x%5 == 3:
-        putzliste[2].append(x)
-    elif x%5 == 4:
-        putzliste[3].append(x)
-    elif x%5 == 0:
-        putzliste[4].append(x)
+  if x%5 == 1:
+    putzliste[0].append(x)
+  elif x%5 == 2:
+    putzliste[1].append(x)
+  elif x%5 == 3:
+    putzliste[2].append(x)
+  elif x%5 == 4:
+    putzliste[3].append(x)
+  elif x%5 == 0:
+    putzliste[4].append(x)
 
 for x in range(personen):
-    print(putzliste[x])
+  print(putzliste[x])
