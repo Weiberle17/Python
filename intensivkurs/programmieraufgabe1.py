be8d5a8663b1cab6817b4e8416c63bc96008b974
Weiberle17 <maxweiberle63@gmail.com>
2023-03-20 19:42:48 +0100

    final version

--- intensivkurs/programmieraufgabe1.py
+++ intensivkurs/programmieraufgabe1.py
@@ -1,16 +1,37 @@
-string = "Python ist sooo eine tolle Programmiersprache!"
-myDict = {}
-list = []
+# Eingabe des Strings
+s = "Python ist sooo eine tolle Programmiersprache!"
 
-for char in string:
+# Declaration des verwendeten Dictionarys
+myDict = {}
+
+# Declaration des verwendeten Liste
+myList = []
+
+# Declaration 
+sortedList = []
+
+# For loop zum Erstellen des Dictionarys
+for char in s:
+  # If-Schleife kontrolliert, ob Zeichen bereits in Dictionary ist
+  # Wenn nein wird es also Key mit dem Value 1 gespeichert
   if char not in myDict:
     myDict[char] = 1
+  # Wenn ja wird das Value des Zeichens um eins erhöht
   else:
     myDict[char] = myDict[char] + 1
+# Whitespaces sollen nicht beachtet werden, also entfernen wir Sie hier aus dem Dictionary
 myDict.pop(' ')
 
+# For-Loop zum Erstellen einer Liste, die wir sortieren können
 for key, value in myDict.items():
-  list.append((value, key))
-list.sort()
+  myList.append([value, key])
 
-print(list)
+for i in range(3):
+  maximum = myList[0]
+  for x in myList:
+    if x > maximum:
+      maximum = x
+  sortedList.append(maximum)
+  myList.remove(maximum)
+
+print("Die drei häufigsten Zeichen sind {}, {} und {}.".format(sortedList[0][1], sortedList[1][1], sortedList[2][1]))
