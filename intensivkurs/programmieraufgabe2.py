346e771a47f50acc21d53e86dbac1679bd83f820
Weiberle17 <maxweiberle63@gmail.com>
2023-03-21 09:27:40 +0100

    data input working

--- intensivkurs/programmieraufgabe2.py
+++ intensivkurs/programmieraufgabe2.py
@@ -1,14 +1,21 @@
 import argparse
 
 # Argumente parsen. Diese Zeilen dürfen keinesfalls verändert werden! | Parsing arguments. You must not modify these lines!
-parser = argparse.ArgumentParser()
-parser.add_argument("statistic", choices=["avg", "max"], help="Which statistic should be run?")
-parser.add_argument("variable", choices=["distance", "delay"], help="What variable should be used for the calculation?")
-parser.add_argument("tsvfile", help="Name of data file to be analyzed")
-parser.add_argument("--carrier", dest="carrier", help="Airline code of flights to be included")
-parser.add_argument("--date", dest="date", help="Departure date for flights to be included")
-parser.add_argument("--origin", dest="origin", help="Departure origin for flights to be included")
-args = parser.parse_args()
+# parser = argparse.ArgumentParser()
+# parser.add_argument("statistic", choices=["avg", "max"], help="Which statistic should be run?")
+# parser.add_argument("variable", choices=["distance", "delay"], help="What variable should be used for the calculation?")
+# parser.add_argument("tsvfile", help="Name of data file to be analyzed")
+# parser.add_argument("--carrier", dest="carrier", help="Airline code of flights to be included")
+# parser.add_argument("--date", dest="date", help="Departure date for flights to be included")
+# parser.add_argument("--origin", dest="origin", help="Departure origin for flights to be included")
+# args = parser.parse_args()
 
 # Ab hier die Lösung programmieren | Solution programming starts here ....
 
+import pandas as pd
+
+flights = pd.read_csv("flights.tsv", delim_whitespace=True)
+# flights = pd.read_csv(input("Geben Sie den Namen der Datei an"))
+flights.head()
+
+print(flights.columns)
